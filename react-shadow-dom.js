84f14a71054642e0218dc9efdd6ac6d6a33db5e2
izakharchenko@exacttarget.com <3axap4eHko@gmail.com>
2016-01-06 12:21:14 -0500

    bower

--- react-shadow-dom.js
+++ react-shadow-dom.js
@@ -1,7 +1,7 @@
-/*! React Shadow DOM v 0.0.1 | Copyright (c) 2015 Ivan (3axap4eHko) Zakharchenko*/
+/*! React Shadow DOM v 0.0.3 | Copyright (c) 2016 Ivan (3axap4eHko) Zakharchenko*/
 'use strict';
 
-( global => {
+( function(global) {
     var React;
     var ReactDOM;
     if (typeof require === 'function') {
@@ -11,8 +11,12 @@
         React = global.React;
         ReactDOM = global.ReactDOM;
     }
-    function attributesToObject(element){
-        return Array.from(element.attributes).reduce( (data, attr) => (data[attr.name]=attr.value,data), {});
+
+    function aggregate(data, attr) {
+        return (data[attr.name]=attr.value,data);
+    }
+    function attributesToObject(element) {
+        return Array.from(element.attributes).reduce(aggregate, {});
     }
     function register(name, componentClass, onRender) {
         var component = Object.create(HTMLElement.prototype);
@@ -26,7 +30,7 @@
         return componentClass;
     }
     React.registerShadow = register;
-    React.createShadowClass = (name, spec) => {
+    React.createShadowClass = function (name, spec) {
         return register(name, React.createClass(spec));
     };
 })(typeof global == 'object' ? global : typeof window == 'object' ? window : this);
